69181798ca439699209f3d4081bcee92a86c9d5b
ochi12 <kaurichannel@gmail.com>
2025-05-07 18:27:49 +0800

    remove unused imports

--- ubuntunetspeed@ochi12.github.com/extension.js
+++ ubuntunetspeed@ochi12.github.com/extension.js
@@ -31,8 +31,6 @@ import {
   Extension,
   gettext as _,
 } from "resource:///org/gnome/shell/extensions/extension.js";
-import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
-import * as PopupMenu from "resource:///org/gnome/shell/ui/popupMenu.js";
 
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
